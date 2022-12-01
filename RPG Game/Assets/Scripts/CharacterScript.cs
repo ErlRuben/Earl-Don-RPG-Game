033d001a7a5e8efb932b6bf1531a694451877c82
Earl Batenga <61070797+ErlRuben@users.noreply.github.com>
2022-12-01 17:06:05 +0800

    2nd Push
    Change Char Anim&Cont

--- RPG Game/Assets/Scripts/CharacterScript.cs
+++ RPG Game/Assets/Scripts/CharacterScript.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    public float speed =1f;
+    private CharacterController characterController;
     public Animator anim;
+    [SerializeField] private float moveSpeed;
+    [SerializedField] private float walkSpeed;
+    [SerializedField] private float runSpeed;
+
+    private  Vector3 moveDirection;
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
     }
 
@@ -18,7 +24,7 @@ public class CharacterScript : MonoBehaviour
         if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.Space)){
             anim.SetInteger("BasicMovement", 6);
         }
-        else if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)){
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)){
             anim.SetInteger("BasicMovement", 2);
         }
         else if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftControl)){
@@ -46,13 +52,37 @@ public class CharacterScript : MonoBehaviour
         else if(Input.GetKey(KeyCode.D)){
             anim.SetInteger("BasicMovement", 1);
         }
-        else if(Input.GetKey(KeyCode.Space)){
-            anim.SetInteger("BasicMovement", 7);
+        else if(Input.GetKeyDown(KeyCode.Space)){
+            //anim.SetInteger("BasicMovement", 7);
+            Debug.Log("Press Space");
+            velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
         }
         else{
             anim.SetInteger("BasicMovement", 0);
+        }   
+    }
+    private void Move() {
+        float moveZ = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(0,0,moveZ);
+        moveDirection += walkSpeed;
+        if(moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift)){
+            Walk();
         }
-
+        else if(moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift)){
+            Run();
+        }
+        else if(moveDirection==Vector3.zero){
+            Idle();
+        }
+        characterController.Move(moveDirection*Time.deltaTime);
+    }
+    private void Idle(){
         
+    }
+    private void Walk(){
+        moveSpeed = walkSpeed;
+    }
+    private void Run(){
+        moveSpeed = runSpeed;
     }
 }
